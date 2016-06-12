319af037c912be483e34e512753c69e706a2d2f2
oudomseila <oudomseilaphok@gmail.com>
2016-06-12 11:48:12 +0700

    adjust dashboard

--- js/factories/ConstantFactory.js
+++ js/factories/ConstantFactory.js
@@ -5,7 +5,8 @@ angular
     .module('TK-WEB-PITCH')
     .factory('ConstantFactory', function(){
 
-            var API_URL = 'http://192.168.1.100:8888/TK-API/';
+            //var API_URL = 'http://192.168.1.100:8888/TK-API/';
+            var API_URL = 'http://10.10.10.242:8888/TK-API/';
             //  return $http.get('http://192.168.1.100:8888/TK-API/sportclubs.json', config);
         return {
             API_URL: API_URL
