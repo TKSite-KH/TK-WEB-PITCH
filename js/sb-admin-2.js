7dc4272d2239cb782b3dbc47ede5fb7b9338093d
oudomseila <oudomseilaphok@gmail.com>
2016-06-13 13:00:03 +0700

    refactor code

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -17,6 +17,7 @@ $(function() {
         } else {
             $('div.navbar-collapse').removeClass('collapse')
         }
+
         height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
         height = height - topOffset;
         if (height < 1) height = 1;
